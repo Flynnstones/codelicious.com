290031c5b2563aabab21b71833e5214adfc5faad
Flynnstones <filocasoni1994@gmail.com>
2023-12-20 01:49:55 +0100

    file3

--- file3.c
+++ file3.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
-void	stampa(int tabella[], int tagliaTabella); // oppure stampa(int *tabella, int tagliaTabella)
+#include "file3.h"
 
 int	main(int argc, char *argv[])
 {
 	int tabella[4];
+	int somma;
+	double media;	
 
 	tabella[0] = 0;
 	tabella[1] = 1;
@@ -12,6 +13,12 @@ int	main(int argc, char *argv[])
 	tabella[3] = 3;
 
 	stampa(tabella, 4);
+
+	somma = sommaTabella(tabella, 4);
+	printf("La somma dei valori presenti nella variabile 'tabella' è uguale a %d\n", somma);
+	
+	media = mediaTabella(tabella, 4);
+	printf("La media dei valori presenti nella variabile 'tabella' è uguale a %f\n", media);
 	return(0);
 }
 
@@ -25,4 +32,37 @@ void    stampa(int tabella[], int tagliaTabella)
 		printf("Valore di tabella[%d] è %d\n", i, tabella[i]);
 		i = i + 1;
 	}
+}
+
+int     sommaTabella(int tabella[], int tagliaTabella)
+{
+	int	i;
+	int	somma;
+
+	i = 0;
+	somma = 0;
+	while (i < tagliaTabella)
+	{
+		somma = somma + tabella[i];
+		i = i + 1;
+	}
+	return (somma);
+}
+
+double	mediaTabella(int tabella[], int tagliaTabella)
+{
+	int	i;
+	int	somma;
+	double	media;
+
+	i = 0;
+	media = 0;
+	somma = 0;
+	while (i < tagliaTabella)
+        {
+                somma = somma + tabella[i];
+                i = i + 1;
+        }
+	media = (double)somma / tagliaTabella;
+        return (media);
 }
---
+++ file3.h
@@ -0,0 +1,3 @@
+void    stampa(int *tabella, int tagliaTabella);
+int     sommaTabella(int tabella[], int tagliaTabella);
+double  mediaTabella(int tabella[], int tagliaTabella); 
