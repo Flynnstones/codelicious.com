afd27d40ff61ac883559197565521fe9d4a0f05a
Flynnstones <filocasoni1994@gmail.com>
2023-12-20 12:09:21 +0100

    p.131 ex.03-05

---
+++ tabelle.h
@@ -0,0 +1,3 @@
+void copia(int tabellaOriginale[], int tabellaCopia[], int grandezzaTabella);
+void massimoTabella(int tabella[], int grandezzaTabella, int valoreMax);
+void riordinareTabella(int tabella[], int tagliaTabella);
