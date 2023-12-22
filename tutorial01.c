4a5f738a286d6989720b57fd50fec73f11a6ea39
Flynnstones <filocasoni1994@gmail.com>
2023-12-22 04:10:13 +0100

    p.82 game01

--- tutorial01.c
+++ tutorial01.c
@@ -2,15 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-int	main(int argc, char **argv)
+int	main(int argc, char *argv[])
 {
 	int numeroMisterioso = 0, numeroInserito = 0;
-	const int MAX = 100, MIN = 1;
 	
 	scanf("%d", &numeroMisterioso);
 	system("clear");
-	//srand(time(NULL));
-	//numeroMisterioso = (rand() % (MAX - MIN + 1)) + MIN;
 	while(numeroInserito != numeroMisterioso)
 	{
 		printf("Indovina il numero? ");
