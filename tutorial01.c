342e6d948f0471e101697e0e98d9d6f6e9572c05
Flynnstones <filocasoni1994@gmail.com>
2023-12-22 02:18:54 +0100

    pag.82 game01

---
+++ tutorial01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int	main(int argc, char **argv)
+{
+	int numeroMisterioso = 0, numeroInserito = 0;
+	const int MAX = 100, MIN = 1;
+	
+	srand(time(NULL));
+	numeroMisterioso = (rand() % (MAX - MIN + 1)) + MIN;
+	while(numeroInserito != numeroMisterioso)
+	{
+		printf("Indovina il numero? ");
+        	scanf("%d", &numeroInserito);
+		if(numeroMisterioso < numeroInserito)
+		{
+			printf("-\n");
+		}	
+		else if(numeroMisterioso >  numeroInserito)
+		{
+			printf("+\n");
+		}
+		else
+		{
+                printf("BRAVO!\n");
+        	}
+	}
+	return (0);
+}
